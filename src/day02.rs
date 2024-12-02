7fefbdda47f3e1c5c913e4c1f2199fdde2beb694
Wouter Jehee <wouter@wouterjehee.com>
2024-12-02 10:52:24 +0100

    day2 part 1

--- src/day02.rs
+++ src/day02.rs
@@ -1,21 +1,35 @@
-use std::fs;
+const MAX_DIFF: i32 = 3;
 
-fn part_one() -> i32 {
-    let file_path = "puzzle_inputs/day02.txt";
-    let contents = fs::read_to_string(file_path)
-        .expect("To read the file");
-
-    let reports = contents.split("\n");
+fn part_one(contents: &str) -> i32 {
+    let reports = contents.split("\n").filter(|r| *r != "");
     let mut safe_reports = 0;
     for report in reports {
         let mut prev_level = None;
-        for level in report.split(" ") {
-            let level: i32 = level.parse().expect("Level to be a number");
+        let mut increasing = None;
+        let mut safe = true;
+        for level in report.split_whitespace() {
+            let level: i32 = level.trim().parse().expect("Level to be a number");
             if let Some(prev) = prev_level {
-                prev_level = Some(level);
-            }
+                let diff: i32 = prev - level;
+                if diff == 0 || diff.abs() > MAX_DIFF {
+                    safe = false;
+                    break;
+                }
+                if let Some(inc) = increasing {
+                    if (inc && diff < 0) || (!inc && diff > 0) {
+                        safe = false;
+                        break;
+                    } 
+                } else {
+                    if prev < level { increasing = Some(false) } else if prev > level { increasing = Some(true) } else { 
+                        safe = false;
+                        break;
+                    }
+                }
+            } 
+            prev_level = Some(level);
         }
-        safe_reports += 1;
+        if safe { safe_reports += 1; }
     }
     safe_reports
 }
@@ -23,12 +37,22 @@ fn part_one() -> i32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
 
     #[test]
-    fn day1() {
-        assert_eq!(part_one(), 0);
+    fn example() {
+        let file_path = "examples/day02.txt";
+        let contents = fs::read_to_string(file_path)
+            .expect("To read the file");
+        assert_eq!(part_one(&contents), 2);
+    }
+
+    #[test]
+    fn input() {
+        let file_path = "puzzle_inputs/day02.txt";
+        let contents = fs::read_to_string(file_path)
+            .expect("To read the file");
+        assert_eq!(part_one(&contents), 442);
     }
 }
-
-
 
